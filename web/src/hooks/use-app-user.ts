988eac6a0e925800157051730b3a9f42d1e6762d
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-12 15:52:54 +0100

    fix

---
+++ web/src/hooks/use-app-user.ts
@@ -0,0 +1,30 @@
+import { UserProfile, useUser } from "@auth0/nextjs-auth0";
+import { User } from "@studio/schema";
+import { getClient } from "lib/sanity.server";
+import { groq } from "next-sanity";
+import { useCallback, useEffect, useState } from "react";
+
+export function useAppUser() {
+  const { user: authUser } = useUser();
+
+  const [user, setUser] = useState<User | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
+
+  const query = groq`*[_type == "user" && auth0_sub == $sub][0]`;
+
+  useEffect(() => {
+    if (authUser?.sub) {
+      setIsLoading(true);
+      getClient()
+        .fetch<User>(query, { sub: authUser?.sub })
+        .then((res) => {
+          setUser(res);
+        })
+        .catch(() => setError("Could not fetch user."))
+        .finally(() => setIsLoading(false));
+    }
+  }, [authUser?.sub]);
+
+  return { user, isLoading, error };
+}
