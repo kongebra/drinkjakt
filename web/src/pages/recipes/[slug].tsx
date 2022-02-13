fe779bda8992d878e67b029b053d14bdb12a5e03
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-13 10:58:25 +0100

    web

--- web/src/pages/recipes/[slug].tsx
+++ web/src/pages/recipes/[slug].tsx
@@ -11,49 +11,9 @@ import { getClient } from "lib/sanity.server";
 
 import { Rating, Recipe } from "@studio/schema";
 
-function filterDataToSingleItem<T extends { _id: string }>(
-  data: T | Array<T>,
-  preview: boolean
-): T {
-  if (!Array.isArray(data)) {
-    return data;
-  }
+interface Props {}
 
-  if (data.length === 1) {
-    return data[0];
-  }
-
-  if (preview) {
-    return data.find((item) => item._id.startsWith(`drafts.`)) || data[0];
-  }
-
-  return data[0];
-}
-
-interface Props {
-  recipe: Recipe;
-  ratings: Array<Rating>;
-}
-
-const RecipePage: React.FC<Props> = ({ recipe, ratings }) => {
-  const router = useRouter();
-
-  const averageRating = React.useMemo<number>(() => {
-    if (ratings.length === 0) {
-      return 0;
-    }
-
-    const sum = ratings.reduce((prev, curr) => {
-      return prev + (curr?.rating || 0);
-    }, 0);
-
-    return sum / ratings.length;
-  }, [ratings]);
-
-  React.useEffect(() => {
-    console.log({ recipe, ratings });
-  }, []);
-
+const RecipePage: React.FC<Props> = ({}) => {
   return <></>;
 };
 
@@ -67,40 +27,33 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-interface IParams extends ParsedUrlQuery {
-  slug: string;
-}
-
-export const getStaticProps: GetStaticProps<Props> = async ({
-  params,
-  preview = false,
-}) => {
-  const query = groq`*[_type == "recipe" && slug.current == $slug]`;
-  const queryParams = { slug: (params as IParams).slug };
-
-  const data = await getClient(preview).fetch<Recipe>(query, queryParams);
-
-  if (!data) {
-    return {
-      notFound: true,
-    };
-  }
-
-  const recipe = filterDataToSingleItem(data, preview);
-
-  const ratingQuery = groq`*[_type == "rating" && recipe._ref == $id][]`;
-  const ratingQueryParams = { id: recipe._id };
-
-  const ratings = await getClient(preview).fetch<Array<Rating>>(
-    ratingQuery,
-    ratingQueryParams
-  );
+export const getStaticProps: GetStaticProps = (context) => {
+  const query = groq`*[_type == "recipe" && slug.current == $slug][0] {
+    _id,
+    name,
+    slug,
+    difficulty,
+    glass->{
+    name,
+    slug,
+  },
+    ice->{
+      name,
+      slug
+    },
+    image,
+    ingredients[] {
+      ingredient->{
+      name,
+      slug,
+    },
+      amount,
+      unit
+    },
+  }`;
 
   return {
-    props: {
-      recipe,
-      ratings: ratings || [],
-    },
+    props: {},
   };
 };
 
