f449b6d634d38cd1d28e595b63bfaf05812ab21b
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-14 14:30:35 +0100

    change color of footer

--- web/src/layouts/Footer.tsx
+++ web/src/layouts/Footer.tsx
@@ -26,16 +26,16 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="bg-white">
+    <footer className="bg-dark text-light">
       <div className="container">
         <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="col-md-4 d-flex align-items-center">
             <Link href="/" passHref>
-              <a className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+              <a className="mb-3 me-2 mb-md-0 text-light text-decoration-none lh-1">
                 <strong>DrinkJakt</strong>
               </a>
             </Link>
-            <span className="text-muted">
+            <span>
               &copy; {new Date().getFullYear()}{" "}
               <a href="https://kongebra.net" target="_blank">
                 kongebra.net
@@ -46,7 +46,7 @@ const Footer = () => {
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             {socials.map((item, index) => (
               <li className="ms-3" key={index}>
-                <a href={item.href} target="_blank" className="text-muted">
+                <a href={item.href} target="_blank">
                   {item.icon}
                 </a>
               </li>
