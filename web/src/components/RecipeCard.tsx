c9b9b7c04bf25834803f42ec1bc86815140adec5
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-16 15:43:20 +0100

    refactor to tailwind

--- web/src/components/RecipeCard.tsx
+++ web/src/components/RecipeCard.tsx
@@ -1,14 +1,24 @@
-import { Recipe } from "@studio/schema";
+import { Recipe, RecipeDetails } from "@studio/schema";
 import { getClient } from "lib/sanity.server";
 import { useNextSanityImage } from "next-sanity-image";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import React, { useMemo } from "react";
 
-import { FaHeart, FaRegHeart } from "react-icons/fa";
+import {
+  FaHeart,
+  FaListUl,
+  FaRegHeart,
+  FaRegStar,
+  FaStar,
+  FaStarHalfAlt,
+  FaTachometerAlt,
+} from "react-icons/fa";
+import { formatDifficulty } from "utils";
 import FavoriteButton from "./FavoriteButton";
 
 export interface RecipeCardProps {
-  recipe: Recipe;
+  recipe: RecipeDetails;
 
   highRes?: boolean;
 
@@ -38,31 +48,96 @@ const RecipeCard: React.FC<RecipeCardProps> = ({
     },
   });
 
+  const rating = useMemo(() => {
+    if (recipe.ratings) {
+      const sum = recipe.ratings?.reduce(
+        (prev, curr) => prev + (curr.rating || 0),
+        0
+      );
+
+      return sum / recipe.ratings?.length;
+    }
+
+    return 0;
+  }, [recipe.ratings?.length]);
+
+  const renderRating = () => {
+    if (recipe.ratings) {
+      return (
+        <div className={`flex gap-1 text-sky-500 mb-2`}>
+          {[0, 1, 2, 3, 4].map((value) => {
+            const key = `${recipe._id}_${value}`;
+
+            const roundedRating = Math.round(rating * 2) / 2;
+
+            if (roundedRating - value >= 1) {
+              return <FaStar key={key} />;
+            }
+
+            if (roundedRating - value === 0.5) {
+              return <FaStarHalfAlt key={key} />;
+            }
+
+            return <FaRegStar key={key} />;
+          })}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
-    <div className="card mb-3">
-      <div className="card-img-top position-relative">
-        <Image {...imageProps} layout="responsive" unoptimized />
+    <Link href={`/recipes/${recipe?.slug?.current}`}>
+      <a
+        title={recipe.name}
+        className="bg-white rounded shadow overflow-hidden cursor-pointer mb-3 group"
+      >
+        <div className="max-h-40 relative overflow-hidden">
+          <Image
+            {...imageProps}
+            alt={recipe.name}
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
 
-        {showFavorite && (
-          <div
-            className="position-absolute"
-            style={{
-              top: "1rem",
-              right: "1rem",
-            }}
+        <div className="p-3">
+          {renderRating()}
+
+          <h2
+            className={`text-lg font-bold mb-3 group-hover:text-sky-500 group-hover:underline`}
           >
-            <FavoriteButton
-              active={favorite}
-              onClick={onClickFavorite && onClickFavorite}
-            />
-          </div>
-        )}
-      </div>
+            {recipe.name}
+          </h2>
 
-      <div className="card-body">
-        <h5 className="card-title">{recipe.name}</h5>
-      </div>
-    </div>
+          <dl className={`flex flex-wrap gap-3 mb-0 text-sm font-light `}>
+            <dd className="flex gap-1 align-items-center">
+              <FaTachometerAlt />
+              <span className="leading-none">
+                {formatDifficulty(recipe.difficulty, true)}
+              </span>
+            </dd>
+
+            <dd className="flex gap-1 align-items-center">
+              <FaListUl />
+              <span className="leading-none">
+                {recipe.ingredients?.length || 0} ingredienser
+              </span>
+            </dd>
+
+            {recipe.ingredients?.map((ingredient, index) => (
+              <dt
+                key={`${ingredient.ingredient?._id}_${index}`}
+                className="hidden"
+              >
+                {ingredient.ingredient?.name}
+              </dt>
+            ))}
+          </dl>
+        </div>
+      </a>
+    </Link>
   );
 };
 
