14480a0cfcaab9680531a44d7c28df8b251190b3
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-19 23:09:22 +0100

    added temporary pages

--- web/src/pages/recipes/index.tsx
+++ web/src/pages/recipes/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const RecipesPage = () => {
-  return <div>RecipesPage</div>;
+  return (
+    <div className="container mx-auto flex justify-center items-center grow">
+      <h1 className="text-5xl">Oversikt over alle oppskrifter kommer!</h1>
+    </div>
+  );
 };
 
 export default RecipesPage;
--- web/src/pages/search.tsx
+++ web/src/pages/search.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <div className="container mx-auto flex justify-center items-center grow">
+      <h1 className="text-5xl">Søkeresultater kommer straks!</h1>
+    </div>
+  );
 };
 
 export default SearchPage;
