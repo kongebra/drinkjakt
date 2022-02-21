eaac8e2deb94c744478f23beeee6c660f12bebb6
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-21 22:53:04 +0100

    remove regex for email

--- studio/schemas/user.js
+++ studio/schemas/user.js
@@ -24,14 +24,6 @@ export default {
       name: "email",
       title: "Email",
       type: "string",
-      validation: (Rule) =>
-        Rule.regex(
-          /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
-          {
-            name: "email", // Error message is "Does not match email-pattern"
-            invert: false, // Boolean to allow any value that does NOT match pattern
-          }
-        ),
       codegen: { required: true },
     },
     {
