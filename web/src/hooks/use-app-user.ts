e29726c75a65a7c48b4c7d703aaebbb790a6e0f8
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-02-22 21:57:36 +0100

    add subscription to user data

--- web/src/hooks/use-app-user.ts
+++ web/src/hooks/use-app-user.ts
@@ -2,7 +2,7 @@ import { useUser } from "@auth0/nextjs-auth0";
 import { User } from "schema";
 import { getClient } from "lib/sanity.server";
 import { groq } from "next-sanity";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export function useAppUser() {
   const { user: authUser } = useUser();
@@ -12,20 +12,27 @@ export function useAppUser() {
   const [error, setError] = useState<string | undefined>();
 
   const query = groq`*[_type == "user" && auth0_sub == $sub][0]`;
+  const params = useMemo(() => ({ sub: authUser?.sub || "" }), [authUser?.sub]);
 
   useEffect(() => {
-    if (authUser?.sub) {
-      setIsLoading(true);
+    const subsription = getClient()
+      .listen<User>(query, params)
+      .subscribe((update) => {
+        setUser(update.result);
+      });
 
-      getClient()
-        .fetch<User>(query, { sub: authUser?.sub })
-        .then((res) => {
-          setUser(res);
-        })
-        .catch(() => setError("Could not fetch user."))
-        .finally(() => setIsLoading(false));
-    }
-  }, [authUser?.sub, query]);
+    return () => {
+      subsription.unsubscribe();
+    };
+  }, [query, authUser, params]);
+
+  useEffect(() => {
+    getClient()
+      .fetch<User>(query, params)
+      .then(setUser)
+      .catch(() => setError("Could not fetch user."))
+      .finally(() => setIsLoading(false));
+  }, [params, query]);
 
   return { user, isLoading, error };
 }
