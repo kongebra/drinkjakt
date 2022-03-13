3e1a567e9435fb127fa705efdeaeca6e91c4822e
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-03-13 16:00:33 +0100

    validation

--- studio/schemas/user.js
+++ studio/schemas/user.js
@@ -24,6 +24,7 @@ export default {
       name: "email",
       title: "Email",
       type: "string",
+      validation: (Rule) => Rule.required(),
       codegen: { required: true },
     },
     {
