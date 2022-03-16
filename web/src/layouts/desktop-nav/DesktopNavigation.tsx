400f8ea71651a5713f0c173c32ca2c35b5f6981a
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-03-16 21:39:26 +0100

    Implement desktop navigation

--- web/src/layouts/desktop-nav/DesktopNavigation.tsx
+++ web/src/layouts/desktop-nav/DesktopNavigation.tsx
@@ -1,15 +1,57 @@
-import { NavItem } from "layouts/Navigation";
 import React from "react";
+
+import Link from "next/link";
+
+import { NavItem } from "layouts/Navigation";
+import UserAvatar from "./UserAvatar";
+import { useAppUser } from "hooks";
+import Button from "components/Button";
+import { useRouter } from "next/router";
 
 export interface DesktopNavigationProps {
   navItems: NavItem[];
 }
 
-const DesktopNavigation: React.FC<DesktopNavigationProps> = ({}) => {
+const DesktopNavigation: React.FC<DesktopNavigationProps> = ({ navItems }) => {
+  const router = useRouter();
+  const { user, isFetched } = useAppUser();
+
   return (
     <>
-      <nav className="hidden lg:flex">
-        <p>desktop navigation</p>
+      <nav className="hidden lg:flex items-center justify-between px-8 h-16">
+        <div className="flex gap-8">
+          <Link href="/">
+            <a className="text-2xl">
+              <span className="mr-0.5">Drink</span>
+              <span className="text-sky-500 font-bold">Jakt</span>
+            </a>
+          </Link>
+
+          <div className="flex items-center gap-8">
+            {navItems.map((item) => (
+              <Link key={item.href} href={item.href}>
+                <a className="text-base font-semibold hover:text-sky-500 transition">
+                  {item.text}
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        {isFetched && (
+          <div className="flex gap-4 items-center">
+            <UserAvatar />
+
+            {!user && (
+              <Button
+                color="secondary"
+                onClick={() => router.push("/api/auth/login")}
+              >
+                Logg inn
+              </Button>
+            )}
+          </div>
+        )}
       </nav>
     </>
   );
