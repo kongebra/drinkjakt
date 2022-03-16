df79302b151d2f53ed57b2806209ff7aa146f421
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-03-16 10:19:02 +0100

    refactor

--- web/src/components/FavoriteHeart.tsx
+++ web/src/components/FavoriteHeart.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import React from "react";
 import { FaHeart } from "react-icons/fa";
 
-type Size = "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+type Size = "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl";
 
 export interface FavoriteHeartProps {
   active: boolean;
@@ -11,6 +11,8 @@ export interface FavoriteHeartProps {
   size?: Size;
 
   title?: string;
+
+  className?: string;
 }
 
 const sizeStyle: Record<Size, string> = {
@@ -20,6 +22,8 @@ const sizeStyle: Record<Size, string> = {
   xl: "text-xl",
   "2xl": "text-2xl",
   "3xl": "text-3xl",
+  "4xl": "text-4xl",
+  "5xl": "text-5xl",
 };
 
 const FavoriteHeart: React.FC<FavoriteHeartProps> = ({
@@ -27,14 +31,21 @@ const FavoriteHeart: React.FC<FavoriteHeartProps> = ({
   onClick,
   size = "md",
   title,
+
+  className,
 }) => {
   const baseStyle =
     "hover:text-rose-500 focus:text-rose-800 cursor-pointer transition ease-in-out duration-300";
 
-  const heartClassName = clsx(baseStyle, sizeStyle[size], {
-    "text-rose-500": active,
-    "text-slate-500": !active,
-  });
+  const heartClassName = clsx(
+    baseStyle,
+    sizeStyle[size],
+    {
+      "text-rose-500": active,
+      "text-slate-500": !active,
+    },
+    className
+  );
 
   return (
     <button onClick={onClick} title={title} aria-label={title}>
