eb16b85e09c26aa902322cbf07e119f1aca7c367
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-03-16 10:19:33 +0100

    refactor frontpage

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,33 +1,17 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 
-import { groq } from "next-sanity";
-
-import type { FrontpageWithRecipes } from "schema";
-
 import { getClient } from "lib/sanity.server";
 
-import RecipeCard from "components/RecipeCard";
-
-import { useFavorites } from "hooks";
-import { useEffectOnce } from "usehooks-ts";
 import { featuredRecipesQuery, FeaturedRecipesQueryResponse } from "queries";
 
+import FrontpageRecipe from "components/FrontpageRecipe";
+
 interface Props {
-  frontpage: FrontpageWithRecipes;
+  frontpage: FeaturedRecipesQueryResponse;
 }
 
 const Home: NextPage<Props> = ({ frontpage }) => {
-  const { isFavorite, toggleFavorite } = useFavorites();
-
-  useEffectOnce(() => {
-    getClient()
-      .fetch<FeaturedRecipesQueryResponse>(featuredRecipesQuery)
-      .then((data) => {
-        console.table(data.featured_recipes);
-      });
-  });
-
   return (
     <>
       <Head>
@@ -35,78 +19,26 @@ const Home: NextPage<Props> = ({ frontpage }) => {
       </Head>
 
       <section>
-        <div className="container mx-auto pt-10 pb-5">
-          <h1 className="text-6xl text-center font-bold mb-10">
+        <div className="container mx-auto pt-10 pb-5 px-4">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-bold mb-10">
             Fremhevede drinker
           </h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-5 sm:px-0 gap-5">
-            {frontpage.recipes?.slice(0, 4).map((recipe) => {
-              const favorite = isFavorite(recipe);
-
-              return (
-                <RecipeCard
-                  key={recipe._id}
-                  recipe={recipe}
-                  favorite={favorite}
-                  onClickFavorite={() => {
-                    toggleFavorite(recipe);
-                  }}
-                />
-              );
-            })}
+          <div className="flex flex-col gap-4">
+            {frontpage.featured_recipes.map((recipe) => (
+              <FrontpageRecipe key={recipe._id} recipe={recipe} />
+            ))}
           </div>
-        </div>
-      </section>
-
-      <section className="bg-white">
-        <div className="container mx-auto pt-10 pb-5">
-          <h2 className="text-5xl text-center font-bold mb-10">
-            {`Juicer & Sirup`}
-          </h2>
-
-          <div className="grid grid-cols-4 gap-5"></div>
         </div>
       </section>
     </>
   );
 };
 
-const query = groq`*[_type == "frontpage"][0] {
-  "recipes": featured_recipes[]->{
-    _id,
-    name,
-    slug,
-
-    image,
-    ingredients[] {
-      ingredient->{
-        name,
-        slug,
-      },
-      amount,
-      unit
-    },
-
-    glass->{
-      name,
-      slug,
-    },
-    ice->{
-      name,
-      slug
-    },
-    
-    "ratings": *[_type == "rating" && recipe._ref == ^._id] {
-      rating
-    }
-  },
-}`;
-
-export const getStaticProps: GetStaticProps<Props> = async ({
-  preview = false,
-}) => {
-  const frontpage = await getClient(preview).fetch<FrontpageWithRecipes>(query);
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const frontpage = await getClient().fetch<FeaturedRecipesQueryResponse>(
+    featuredRecipesQuery
+  );
 
   if (!frontpage) {
     return {
